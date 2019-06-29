19fc75b402fedeb588d1a8754e74f0ddbf48d2bd
lendle <lendle_tseng@seed.net.tw>
2019-06-29 15:00:28 +0800

    step 4

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const {Iris}=require('machinelearn/datasets');
 const {KNeighborsClassifier}=require('machinelearn/neighbors');
+const { train_test_split }=require('machinelearn/model_selection');
 
 (async function(){
     const iris=new Iris();
     const d=await iris.load();
     const knn=new KNeighborsClassifier();
-    knn.fit(d.data, d.targets);
+
+    let split=train_test_split(d.data, d.targets, {
+        test_size: 0.1,
+        train_size: 0.9,
+        random_state: Math.random()*100
+    });
+
+    knn.fit(split.xTrain, split.yTrain);
     let correct=0.0;
-    for(let i=0; i<d.data.length; i++){
-        if(d.targets[i]==knn.predict(d.data[i])){
+    for(let i=0; i<split.xTest.length; i++){
+        let result=knn.predict(split.xTest[i]);
+        if(split.yTest[i]==result){
             correct=correct+1;
         }
-        console.log(d.targets[i], knn.predict(d.data[i]));
     }
-    console.log(correct/d.data.length);
+    console.log(correct/split.xTest.length);
 })();

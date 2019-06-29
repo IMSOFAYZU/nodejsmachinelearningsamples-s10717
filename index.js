fb5c0a55a0cbbad6bcec184a994e4ed5dac930e1
lendle <lendle_tseng@seed.net.tw>
2019-06-29 14:21:58 +0800

    step 2

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const {KNeighborsClassifier}=require('machinelearn/neighbors');
 (async function(){
     const iris=new Iris();
     const d=await iris.load();
+    const knn=new KNeighborsClassifier();
+    knn.fit(d.data, d.targets);
+    for(let i=0; i<d.data.length; i++){
+        console.log(d.targets[i], knn.predict(d.data[i]));
+    }
 })();

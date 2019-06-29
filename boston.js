4e6e0696be325dbc24598cf50fa0b0e4c19516c0
lendle <lendle_tseng@seed.net.tw>
2019-06-29 21:31:21 +0800

    step 6

--- boston.js
+++ boston.js
@@ -5,5 +5,11 @@ const { train_test_split }=require('machinelearn/model_selection');
 (async function(){
     const boston=new Boston();
     const d=await boston.load();
-    console.log(d.data);
+    const ln=new LinearRegression();
+    let split=train_test_split(d.data, d.targets, {
+        test_size: 0.1,
+        train_size: 0.9,
+        random_state: Math.random()*100
+    });
+    
 })();

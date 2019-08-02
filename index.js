12ffb000181e6682d4befc08bd07a569e3aa2ca5
s10717 <52558732+s10717@users.noreply.github.com>
2019-08-02 14:15:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const {Iris}=require('machinelearn/datasets');
 const {KNeighborsClassifier}=require('machinelearn/neighbors');
-
+const { train_test_split }=require('machinelearn/model_selection');
 (async function(){
     const iris=new Iris();
     const d=await iris.load();
     const knn=new KNeighborsClassifier();
+    let s=train_test_split(d.data,d.targets,{
+            test_size:0.1,train_size:0.9,random_state:50
+    });
     knn.fit(d.data, d.targets);
-    let correct=0.0;
+    let count=0;
     for(let i=0; i<d.data.length; i++){
-        if(d.targets[i]==knn.predict(d.data[i])){
-            correct=correct+1;
+        let y=d.targets[i];
+        let p=kmm.predict(d.data[i]);
+        if(y==p){
+            count++;
         }
-        console.log(d.targets[i], knn.predict(d.data[i]));
     }
-    console.log(correct/d.data.length);
+    console.log(count/s.xTest.length);
 })();
